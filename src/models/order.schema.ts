c037256ba8eb3c53b4ec7b7f4570eb828493e28d
AbdulazizRus <coderdim@gmail.com>
2024-07-19 09:29:58 +0500

    changes

---
+++ src/models/order.schema.ts
@@ -0,0 +1,23 @@
+import * as mongoose from 'mongoose';
+
+export const OrderSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    totalPrice: String,
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+        },
+        quantity: Number,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
---
+++ src/models/product.schema.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+
+export const ProductSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    image: String,
+    price: String,
+    amount: Number,
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
