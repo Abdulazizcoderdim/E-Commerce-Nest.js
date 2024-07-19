70e4490536970797dbe8792159e25e5f4e9fec5d
AbdulazizRus <coderdim@gmail.com>
2024-07-19 09:02:06 +0500

    changes

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://coderdim:g9Er9zY3i2T8Owkg@cluster0.uuicts2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
